d6cad57a4dfed48dc70e3881a754fe2862ea2b5d
Vincent77 <wentianqi77@outlook.com>
2018-12-02 21:12:00 +0800

    seperate routes

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ const rateLimit = require('express-rate-limit')
 const cors = require('cors')
 
 const { PORT } = require('./consts')
-const store = require('./store')
-const { initialNavigation } = require('./store')
-const {
-  fetchData,
-  getPosts,
-  generateAnchors,
-  formatDate,
-} = require('./services')
+const { fetchData } = require('./services')
+const routes = require('./routes')
 
 const app = express()
 
@@ -22,36 +16,10 @@ app.use(rateLimit({
   max: 150,
 }))
 
+app.use(routes)
+
 app.set('views', './views')
 app.set('view engine', 'pug')
-
-app.get('/', (req, res) => {
-  const page = req.query.page || 1
-  const { type, search } = req.query
-  store.posts = getPosts(page, type, search)
-  store.navigation = initialNavigation
-
-  res.render('index', store)
-})
-
-app.get('/search', (req, res) => {
-  store.posts = getPosts(1, null, req.query.content)
-  store.navigation = initialNavigation
-
-  res.render('index', store)
-})
-
-app.get('/posts/:id', (req, res) => {
-  store.post = JSON.parse(store.bufferPosts.toString()).filter(item => String(item.id) === req.params.id)[0]
-  store.post.updated_at = formatDate(store.post.updated_at)
-  store.navigation = generateAnchors(store.post.body)
-
-  res.render('Post', store)
-})
-
-app.get('*', (req, res) => {
-  res.render('404', store)
-})
 
 setInterval(() => {
   fetchData()
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const store = require('../store')
+const { initialNavigation } = require('../store')
+const { getPosts, generateAnchors, formatDate } = require('../services')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  const page = req.query.page || 1
+  const { type, search } = req.query
+  store.posts = getPosts(page, type, search)
+  store.navigation = initialNavigation
+
+  res.render('index', store)
+})
+
+app.get('/search', (req, res) => {
+  store.posts = getPosts(1, null, req.query.content)
+  store.navigation = initialNavigation
+
+  res.render('index', store)
+})
+
+app.get('/posts/:id', (req, res) => {
+  store.post = JSON.parse(store.bufferPosts.toString()).filter(item => String(item.id) === req.params.id)[0]
+  store.post.updated_at = formatDate(store.post.updated_at)
+  store.navigation = generateAnchors(store.post.body)
+
+  res.render('Post', store)
+})
+
+app.get('*', (req, res) => {
+  res.render('404', store)
+})
+
+module.exports = app
