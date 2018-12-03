19a4965dc91083f7608be4382caf00ea96865326
vwen <Vincent.Wen@adp.com>
2018-12-03 14:07:47 +0800

    style

--- consts.js
+++ consts.js
@@ -1,4 +1,4 @@
-const API_POSTS = 'https://api.github.com/repos/77Vincent/blog/issues'
+const API_POSTS = 'https://api.github.com/repos/77Vincent/77vincent/issues'
 const QUERY_LIMITS = 20
 const PORT = process.env.PORT || 3000
 
