f39f10721988f66aa89988ec55d102becb2d32f7
fr-wentianqi <tianqi.wen@fastretailing.com>
2019-10-09 12:26:49 +0800

    stype update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "77vincent",
-      script: "./index.js",
+      name: '77vincent',
+      script: './index.js',
       watch: true,
       watch_delay: 1000,
-      ignore_watch : ["node_modules", "data", "static"],
+      ignore_watch: ['node_modules', 'static'],
       env: {
-        "PORT": 3000,
-        "NODE_ENV": "development"
+        PORT: 3000,
+        NODE_ENV: 'development',
       },
       env_production: {
-        "PORT": 80,
-        "NODE_ENV": "production"
-      }
-    }
-  ]
+        PORT: 80,
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 }
